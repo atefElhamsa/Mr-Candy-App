d0ef3e5c0026d95ea0322767c53232ae0c6f041e
atefe <atefelhamsa79@gmail.com>
2025-04-15 02:05:09 +0200

    initial translation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -18,27 +19,40 @@ void main() async {
   await Hive.initFlutter();
   await Hive.openBox(AppTexts.nameOfBox);
   await ScreenUtil.ensureScreenSize();
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => BannersCubit()..getBanners(),
-        ),
-        BlocProvider(
-          create: (context) => CategoriesCubit()..getCategories(),
-        ),
-        BlocProvider(
-          create: (context) =>
-              BestSellerProductsCubit()..getBestSellerProducts(),
-        ),
-        BlocProvider(
-          create: (context) => FavouriteCubit(),
-        ),
-        BlocProvider(
-          create: (context) => CartCubit(),
-        ),
+    EasyLocalization(
+      path: "assets/translation",
+      supportedLocales: const [
+        Locale('ar'),
+        Locale('en'),
       ],
-      child: const MyApp(),
+      fallbackLocale: const Locale('ar'),
+      startLocale: Locale(
+        WidgetsBinding.instance.platformDispatcher.locale.languageCode,
+      ),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => BannersCubit()..getBanners(),
+          ),
+          BlocProvider(
+            create: (context) => CategoriesCubit()..getCategories(),
+          ),
+          BlocProvider(
+            create: (context) =>
+                BestSellerProductsCubit()..getBestSellerProducts(),
+          ),
+          BlocProvider(
+            create: (context) => FavouriteCubit(),
+          ),
+          BlocProvider(
+            create: (context) => CartCubit(),
+          ),
+        ],
+        child: const MyApp(),
+      ),
     ),
   );
 }
